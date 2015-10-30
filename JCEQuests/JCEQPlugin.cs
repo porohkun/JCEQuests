6eb7b81e715168b79b4546c4bb864ed2c080535a
Poroh <meta.poroh.kun@gmail.com>
2015-10-30 18:08:40 +0300

    initial commit

---
+++ JCEQuests/JCEQPlugin.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using PNetJson;
+using JsonConfigEditor;
+using JsonConfigEditor.Plugins;
+
+namespace JCEQuests
+{
+    public class JCEQPlugin : IPlugin
+    {
+        public string GetFullName() { return "JCEQuests"; }
+
+        public IEnumerable<ToolStripMenuItem> GetMenuItems()
+        {
+            ToolStripMenuItem item2 = new ToolStripMenuItem("Отладка") { Tag = "{\"enabled\":\"True\"}" };
+            //item2.Click += BuildCards;
+            yield return item2;
+        }
+
+        public IEnumerable<string> GetMasks()
+        {
+            yield return "qst";
+        }
+
+        public TemplateForm OpenFile(string filename)
+        {
+            if (Path.GetExtension(filename) == ".qst")
+            {
+                var form = MainForm.Instance.MakeNewChildForm(typeof(QuestEditorForm), "квест") as QuestEditorForm;
+                form.Open(filename);
+                return form;
+            }
+            return null;
+        }
+
+        //private void BuildCards(object sender, EventArgs e)
+        //{
+        //    bool edited = false;
+        //    foreach (ISaveableForm frm in MainForm.Instance.MdiChildren)
+        //    {
+        //        if (frm.Edited)
+        //        {
+        //            edited = true;
+        //            break;
+        //        }
+        //    }
+        //    if (edited)
+        //        if (MessageBox.Show("Для сборки карт все открытые файлы должны быть сохранены. Сохранить открытые файлы?",
+        //            "Сборка карт", MessageBoxButtons.YesNo) == System.Windows.Forms.DialogResult.Yes)
+        //        {
+        //            MainForm.Instance.SaveAllFiles(this, new EventArgs());
+        //            edited = false;
+        //        }
+        //    if (!edited)
+        //    {
+        //        BuildCardsForm f2 = new BuildCardsForm();
+        //        f2.ShowDialog(MainForm.Instance);
+        //    }
+        //}
+
+        //private void SetsEdit(object sender, EventArgs e)
+        //{
+        //    MainForm.Instance.MakeNewChildForm(typeof(SetsEditForm), "Правка сетов");
+        //    //SetsEditForm sef = new SetsEditForm(toolStrip1, "Правка сетов");
+        //    //sef.MdiParent = this;
+        //    //sef.Show();
+        //}
+
+        public ISettings GetEmptySettings()
+        {
+            return new Settings();
+        }
+
+        public void RestoreSettings(ISettings settings)
+        {
+            Settings.Inst = settings as Settings;
+        }
+    }
+}
---
+++ JCEQuests/QuestEditorForm.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using JsonConfigEditor;
+
+namespace JCEQuests
+{
+    public partial class QuestEditorForm : TemplateForm, ISaveableForm
+    {
+        #region ISaveableForm
+
+        public bool Edited
+        {
+            get { return _Edited; }
+            protected set
+            {
+                if (value != _Edited)
+                {
+                    if (value)
+                    {
+                        SetText(File.Name + "*");
+                    }
+                    else
+                    {
+                        SetText(File.Name);
+                    }
+                    _Edited = value;
+                    ((MainForm)MdiParent).EditedCheck();
+                }
+            }
+        }
+        private bool _Edited;
+        public FilePath File { get; private set; }
+        public bool CanUndo { get; private set; }
+        public bool CanRedo { get; private set; }
+        public bool WasOpen { get; private set; }
+        public void Save() { }
+        public void Close() { }
+
+        #endregion
+
+        public void Open(string filename)
+        {
+            textBox1.Text = filename;
+            File = new FilePath(filename);
+        }
+
+        public QuestEditorForm()
+        {
+            InitializeComponent();
+        }
+
+        public QuestEditorForm(ToolStrip ts, string fileName)
+            : base(ts, fileName)
+        {
+            InitializeComponent();
+            
+        }
+
+    }
+}
